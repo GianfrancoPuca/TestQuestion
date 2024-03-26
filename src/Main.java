ad635b67b7b2c328c83cdaa25b9c5f0c9f386610
GianfrancoPuca <gianfrancopuca@hotmail.it>
2024-03-26 16:29:13 +0100

    ok with the questions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        String q1 = "Di che colore era il cavallo bianco di Napoleone?\n" + "(a)Bianco\n(b)Verde\n(c)Nero\n(d)Rosso\n";
+        String q2 = "Di che colore è la mela?\n" + "(a)Bianco\n(b)Verde\n(c)Nero\n(d)Rosso\n";
+        Question[] questions = {new Question(q1, "a"), new Question(q2, "d")};
+        test(questions);
+    }
+    public static void test(Question[] questions){
+        int score = 0;
+        Scanner keyboardInput = new Scanner(System.in);
+        for(int i = 0; i < questions.length; i++){
+            System.out.println(questions[i].getQuestion());
+            String answer = keyboardInput.nextLine();
+            if(answer.equals(questions[i].getAnswer())){
+                score++;
+            }
+        }
+        System.out.println("You got: " + score + "/" + questions.length);
     }
 }
---
+++ src/Question.java
@@ -0,0 +1,28 @@
+public class Question {
+    private String question;
+    private String answer;
+
+    public Question(){
+        super();
+    }
+    public Question(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
